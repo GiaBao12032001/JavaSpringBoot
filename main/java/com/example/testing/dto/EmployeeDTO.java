4a9b28d6a5909cf3de6ccb7f10975151093ce0c5
GiaBao12032001 <95518513+GiaBao12032001@users.noreply.github.com>
2023-06-26 13:50:18 +0700

    added comment

--- main/java/com/example/testing/dto/EmployeeDTO.java
+++ main/java/com/example/testing/dto/EmployeeDTO.java
@@ -6,7 +6,7 @@ public class EmployeeDTO {
     private int age;
     private String address;
     private double salary;
-    //giabao1
+    //giabao123
     public Long getId() {
         return id;
     }
