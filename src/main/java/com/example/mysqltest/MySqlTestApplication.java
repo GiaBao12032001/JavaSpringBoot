9feef4abca0cf21b23d4fc73c51e7a6d65ab7596
GiaBao12032001 <95518513+GiaBao12032001@users.noreply.github.com>
2023-06-27 15:45:52 +0700

    Fixed typos and unnecessary lines

--- src/main/java/com/example/mysqltest/MySqlTestApplication.java
+++ src/main/java/com/example/mysqltest/MySqlTestApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MysqltestApplication {
+public class MySqlTestApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MysqltestApplication.class, args);
+		SpringApplication.run(MySqlTestApplication.class, args);
 	}
 
 }
