817a9f8fc8dde1a242998bd68a47adbb4cb30205
GiaBao12032001 <95518513+GiaBao12032001@users.noreply.github.com>
2023-06-29 14:01:24 +0700

    Added more exception error messages, and exceptions. Fixed a few SonarLint errors.

--- src/main/java/com/example/mysqltest/exception/ApplicationException.java
+++ src/main/java/com/example/mysqltest/exception/ApplicationException.java
@@ -2,12 +2,10 @@ package com.example.mysqltest.exception;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 @AllArgsConstructor
 @Getter
 public class ApplicationException extends RuntimeException {
-    private int code;
-    private String message;
+    private final int code;
+    private final String message;
 }
